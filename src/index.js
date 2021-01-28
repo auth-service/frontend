c7fadfe0349dac13ee32c1d7e95c9fd0edb8ac92
Ashkan <ashkan65@hotmail.com>
2021-01-28 15:21:44 +0800

    keep for sure

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+// import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <BrowserRouter>
@@ -13,3 +14,4 @@ ReactDOM.render(
 );
 
 serviceWorker.unregister();
+// reportWebVitals();
